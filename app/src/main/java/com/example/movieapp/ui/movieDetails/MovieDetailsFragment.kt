a21c65931ea4330bc5e05295dabb01bc645453e4
Piotr Błochowiak <blochovski@gmail.com>
2023-06-07 15:36:53 +0200

    Add View models Add Movie List Fragment View Model Add Movie Details Fragment View Model Add MovieApplication

--- app/src/main/java/com/example/movieapp/ui/movieDetails/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/movieDetails/MovieDetailsFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.movieapp.databinding.FragmentMovieDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MovieDetailsFragment : Fragment() {
 
     private val binding: FragmentMovieDetailsBinding by lazy { FragmentMovieDetailsBinding.inflate(layoutInflater) }
+    private val viewModel: MovieDetailsFragmentViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
