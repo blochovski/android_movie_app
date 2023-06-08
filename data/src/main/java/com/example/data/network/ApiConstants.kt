06d88ee3f1cb4447ae0cba511be33236b3a8c733
Piotr Błochowiak <blochovski@gmail.com>
2023-06-08 07:35:56 +0200

    Add Movie List to MovieListFragment

--- data/src/main/java/com/example/data/network/ApiConstants.kt
+++ data/src/main/java/com/example/data/network/ApiConstants.kt
@@ -3,7 +3,7 @@ package com.example.data.network.model
 object ApiConstants {
     const val BASE_ENDPOINT = "https://api.themoviedb.org/3/"
     const val NOW_PLAYING_MOVIE_ENDPOINT = "movie/now_playing"
-
+    const val IMAGES_BASE_URL = "https://image.tmdb.org/t/p/w500/"
 }
 
 object ApiParameters {
