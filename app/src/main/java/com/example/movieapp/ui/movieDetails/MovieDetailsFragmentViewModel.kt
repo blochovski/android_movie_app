d29ddc732bc51cbb527d8321456ff98d617bf25d
Piotr Błochowiak <blochovski@gmail.com>
2023-06-08 11:14:07 +0200

    temp commit

--- app/src/main/java/com/example/movieapp/ui/movieDetails/MovieDetailsFragmentViewModel.kt
+++ app/src/main/java/com/example/movieapp/ui/movieDetails/MovieDetailsFragmentViewModel.kt
@@ -1,12 +1,24 @@
 package com.example.movieapp.ui.movieDetails
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.domain.model.movie.EmptyMovie
+import com.example.domain.model.movie.Movie
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieDetailsFragmentViewModel
 @Inject constructor(
-
+    savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
+    private val passedMovie: Movie = savedStateHandle.get<Movie>("movie") ?: EmptyMovie
+    val movie: LiveData<Movie> get() = _movie
+    private val _movie = MutableLiveData<Movie>()
+
+    init {
+        _movie.postValue(passedMovie)
+    }
 }
--- app/src/main/java/com/example/movieapp/ui/movieList/MovieListFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/movieList/MovieListFragment.kt
@@ -6,8 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.domain.model.movie.Movie
 import com.example.movieapp.databinding.FragmentMovieListBinding
+import com.example.movieapp.ui.movieDetails.MovieDetailsFragment
+import com.example.movieapp.ui.movieDetails.MovieDetailsFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,11 +24,11 @@ class MovieListFragment : Fragment() {
         )
     }
     private val viewModel: MovieListFragmentViewModel by viewModels()
+    private val navController: NavController by lazy { findNavController() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initMovieList()
-        observeObservables()
     }
 
     override fun onCreateView(
@@ -31,13 +36,25 @@ class MovieListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View = binding.root
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeObservables()
+    }
+
     private fun observeObservables() {
-        viewModel.movies.observe(this) {
+        viewModel.movies.observe(viewLifecycleOwner) {
             binding.movieList.adapter = MovieListAdapter(it)
+            navigateToMovieDetails(it.first())
         }
     }
 
     private fun initMovieList() = binding.movieList.apply {
         layoutManager = LinearLayoutManager(context)
     }
+
+    private fun navigateToMovieDetails(movie: Movie) = navController.navigate(
+        MovieListFragmentDirections.actionMovieListFragmentToMovieDetailsFragment(
+            movie = movie
+        )
+    )
 }
